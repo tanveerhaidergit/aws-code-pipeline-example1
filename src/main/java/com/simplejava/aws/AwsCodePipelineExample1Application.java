eb2714fbf51ff9ccb4acab050d7d6f13ea9d3789
MOHAMMAD TANVEER HAIDER <mohammadtanveerhaider@MOHAMMADs-MacBook-Pro.local>
2019-10-31 00:56:53 -0400

    aws code pipeline inital commit for elastic beanstalk

---
+++ src/main/java/com/simplejava/aws/AwsCodePipelineExample1Application.java
@@ -0,0 +1,13 @@
+package com.simplejava.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodePipelineExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodePipelineExample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplejava/aws/AwsCodePipelineExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplejava.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipelineExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
